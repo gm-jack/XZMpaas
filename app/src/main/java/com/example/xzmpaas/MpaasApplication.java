167732555e6a7cfe2d9bd177b58a3483390a32f4
gm-jack <15210420307@163.com>
2021-10-20 16:35:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xzmpaas/MpaasApplication.java
@@ -0,0 +1,50 @@
+package com.example.xzmpaas;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.alipay.instantrun.log.Log;
+import com.alipay.mobile.common.logging.api.LoggerFactory;
+import com.alipay.mobile.framework.quinoxless.QuinoxlessApplicationLike;
+
+public class MpaasApplication extends QuinoxlessApplicationLike implements Application.ActivityLifecycleCallbacks {
+    private static final String TAG = "MyApplication";
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        Log.i(TAG, "attacheBaseContext");
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "onCreate");
+        registerActivityLifecycleCallbacks(this);
+    }
+    @Override
+    public void onMPaaSFrameworkInitFinished() {
+        LoggerFactory.getTraceLogger().info(TAG, getProcessName());
+    }
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+        Log.i(TAG, "onActivityCreated");
+    }
+    @Override
+    public void onActivityStarted(Activity activity) {
+    }
+    @Override
+    public void onActivityResumed(Activity activity) {
+    }
+    @Override
+    public void onActivityPaused(Activity activity) {
+    }
+    @Override
+    public void onActivityStopped(Activity activity) {
+    }
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+    }
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+    }}
